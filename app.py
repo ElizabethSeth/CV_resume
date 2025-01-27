770842ee86fee6aa9a4d5866b2e0bab6f6b782b9
Elizabeth Seth <setharth1998@gmail.com>
2025-01-27 21:30:52 +0100

    Some

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ resume_data = {
     "about_me": "Hello! My name is Elizabeth Seth. I am a Data Engineer, Data Analyst, and Data Scientist.",
     "education": "Education:\n- Bachelor's Degree in Law\n- Programming School 42\n- Master's Degree in Data Engineering from Microsoft Simplon.",
     "projects": "Projects:\n1. Portfolio Website\n2. Machine Learning Project.",
-    "skills": "Skills:\n- Python, Kafka, Airflow, ClickHouse, PostgresSQL, MySQL Azure GCP \n- Stress resilience\n- Resourcefulness\n- Enthusiastic engagement in projects.",
+    "skills": "Skills:\n- Python, Kafka, Airflow, ClickHouse, PostgresSQL, MySQL Azure GCP Kubernetis\n- Stress resilience\n- Resourcefulness\n- Enthusiastic engagement in projects.",
     "internships": "Internships:\n- XYZ Corp, Software Intern.\nPernodRicard,DataScientist Aprentice 18 months.",
     "interests": "Interests:\n- Cybersecurity, AI, Open-Source."
 }
